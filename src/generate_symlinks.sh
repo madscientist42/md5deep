62d667068558429939ddc843f8af7f29b8948985
Frank Earl <fearl@freewave.com>
2013-12-09 11:31:30 -0700

    Corrected CMake buildsystem to work with OE correctly...
    The trick I was relying on was busted for OE.  So, we changed it to be a
    series of SYMLINK_APPS list entries and a custom target for each to make
    the symlinks right for install.  Also added a bunch of useful .gitignore
    entries for generated artifacts.

--- src/generate_symlinks.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-rm -f md5deep sha1deep sha256deep tigerdeep whirlpooldeep
-ln -s hashdeep md5deep
-ln -s hashdeep sha1deep
-ln -s hashdeep sha256deep
-ln -s hashdeep tigerdeep
-ln -s hashdeep whirlpooldeep
-
